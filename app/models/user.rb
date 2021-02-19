11402ce93d789276d3c7ea0408416bba980903d6
Mark Force <force.j.mark@gmail.com>
2021-02-19 12:47:14 -0500

    working version of profile page with user posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
  class User < ApplicationRecord
-       has_many :posts
+      has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+      devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 #   # Include default devise modules. Others available are:
 #   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 #   devise :database_authenticatable, :registerable,
 #          :recoverable, :rememberable, :validatable
-
-      
+            # @user = User.find(params[:id])
+            # @posts = @user.posts
 
  end
